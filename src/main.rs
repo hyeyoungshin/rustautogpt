d046af328841094eb50b3f1cafc6557258a9dc63
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-02-19 23:40:00 -0700

    add implementation of database

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,87 @@
 use actix_cors::Cors;
-use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
-use serde::{Deserialize, Serialize};
-use reqwest::Client as HttpClient;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
 use async_trait::async_trait;
+use reqwest::Client as HttpClient;
+use serde::{Deserialize, Serialize};
 
-use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+use std::sync::Mutex;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Task {
     id: u64,
     name: String,
-    completed: bool
+    completed: bool,
 }
-
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: u64,
     username: String,
-    password: String
+    password: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
     tasks: HashMap<u64, Task>,
-    users: HashMap<u64, User>
+    users: HashMap<u64, User>,
+}
+
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    fn get(&self, id: &u64) -> Option<&Task> {
+        self.tasks.get(id)
+    }
+
+    fn get_all(&self) -> Vec<&Task> {
+        self.tasks.values().collect()
+    }
+
+    fn delete(&mut self, id: &u64) {
+        self.tasks.remove(id);
+    }
+
+    fn update(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    // USER DATA RELATED FUNCTIONS
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user_by_name(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|u| u.username == username)
+    }
+
+    // DATABASE Functions
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let data = serde_json::to_string(&self)?; // because of output type ? is appropriate
+        let mut file = fs::File::create("database.json")?;
+        file.write_all(data.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file_content = fs::read_to_string("database.json")?;
+        let db: Self = serde_json::from_str(&file_content)?;
+        Ok(db)
+    }
 }
 
 fn main() {
     println!("Hello, world!");
 }
+
