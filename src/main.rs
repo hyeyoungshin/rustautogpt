284bfc68a57d18c7ae6d1f8e94fa819b68f07c70
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2024-02-19 23:21:44 -0700

    add structs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>
+}
 
 fn main() {
     println!("Hello, world!");
